423f917aed86322b941ed5666c96c5d8675478dc
Avdunusinghe <avdunusinghe@gmail.com>
2022-06-26 08:35:11 +0530

    Database Connection Bug Fixed

--- Initial-Application/Rest-Api/src/utils/database.connection.js
+++ Initial-Application/Rest-Api/src/utils/database.connection.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const databaseConnection = ()=>{
     const connectionString = process.env.connectionString;
-    console.log(connectionString);
     mongoose.connect(connectionString,()=>{
         console.log("Connect Database");
     });
 };
 
-module.exports = {databaseConnection}
+module.exports = databaseConnection
